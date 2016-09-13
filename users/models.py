1601ff455afc7feebdfed448aff402da5e7d10cb
Albert Usaqui <albert.usaqui@blanclink.com>
2016-09-13 16:42:04 -0400

    is_superuser field instead is_staff to use in django admin panel

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class User(AbstractBaseUser):
     full_name = models.CharField(max_length=100)
 
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
