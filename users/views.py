cb1c57aaeae3f519c106cf61bccde111a96a2b14
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-13 16:14:52 -0400

    Add UserAuth which is the main login in the API, it returns the token access, full_name, email and id

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+
+class UserAuth(ObtainAuthToken):
+    throttle_classes = ()
+    permission_classes = ()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response(
+            {
+                'token': token.key,
+                'id': user.id,
+                'full_name': user.full_name,
+                'email': user.email
+            }
+        )
