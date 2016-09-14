eb57cd707cc8a8c924c2fa3c227b1963412b601e
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-14 09:09:49 -0400

    remove comment

--- users/models.py
+++ users/models.py
@@ -16,7 +16,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     full_name = models.CharField(max_length=100)
 
     is_active = models.BooleanField(default=True)
-    # is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
