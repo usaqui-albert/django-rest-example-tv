8495e4a1d99dd054606ca7836b254536815e8df0
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-14 09:05:15 -0400

    new imports to admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-
-from .models import Breeder
+from django.contrib.auth.models import Permission
+from .models import Breeder, User, Veterinarian
 
 admin.site.register(Breeder)
+admin.site.register(Veterinarian)
+admin.site.register(User)
+admin.site.register(Permission)
