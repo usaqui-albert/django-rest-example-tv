3d090f6af7786d7b72ce219db073a83164677d67
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-16 11:48:13 -0400

    Typo and remove unnecessary documentation

--- countries/serializers.py
+++ countries/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Country, State
 
 
-class CountriesSerilizer(serializers.ModelSerializer):
+class CountriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = ('id', 'name', 'code')
--- countries/views.py
+++ countries/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import ListAPIView
 from django.db.models.query import QuerySet
 
 from .models import State, Country
-from .serializers import StateSerializer, CountriesSerilizer
+from .serializers import StateSerializer, CountriesSerializer
 
 
 class StateListView(ListAPIView):
@@ -12,26 +12,6 @@ class StateListView(ListAPIView):
     permission_classes = (permissions.AllowAny,)
 
     def get_queryset(self):
-            """
-            Get the list of items for this view.
-            This must be an iterable, and may be a queryset.
-            Defaults to using `self.queryset`.
-
-            This method should always be used rather than accessing
-            `self.queryset` directly, as `self.queryset` gets evaluated only
-            once, and those results are cached for all subsequent requests.
-
-            You may want to override this if you need to provide different
-            querysets depending on the incoming request.
-
-            (Eg. return a list of items that is specific to the user)
-            """
-            assert self.queryset is not None, (
-                "'%s' should either include a `queryset` attribute, "
-                "or override the `get_queryset()` method."
-                % self.__class__.__name__
-            )
-
             queryset = self.queryset
             if isinstance(queryset, QuerySet):
                 # Ensure queryset is re-evaluated on each request.
@@ -42,5 +22,5 @@ class StateListView(ListAPIView):
 
 class CountryListView(ListAPIView):
     queryset = Country.objects.all()
-    serializer_class = CountriesSerilizer
+    serializer_class = CountriesSerializer
     permission_classes = (permissions.AllowAny,)
