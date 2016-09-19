f7bcf76b1fe4ab38e7bcbb46cd0983483144b90f
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-19 10:36:55 -0400

    added Description to a group

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.models import Permission
-from .models import Breeder, User, Veterinarian
+from .models import Breeder, User, Veterinarian, Group
 
 admin.site.register(Breeder)
 admin.site.register(Veterinarian)
 admin.site.register(User)
 admin.site.register(Permission)
+admin.site.register(Group)
