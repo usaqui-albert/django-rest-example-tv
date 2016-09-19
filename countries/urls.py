74c261887a0c5e0a4f4f8a5245a09d098ec6eba7
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-19 08:57:59 -0400

    Change on contries url

--- countries/urls.py
+++ countries/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import StateListView, CountryListView
 
 urlpatterns = [
-    url(r'^country/(?P<pk>\d+)/states/$', StateListView.as_view()),
-    url(r'^country/$', CountryListView.as_view()),
+    url(r'^(?P<pk>\d+)/states/$', StateListView.as_view()),
+    url(r'^$', CountryListView.as_view()),
 ]
