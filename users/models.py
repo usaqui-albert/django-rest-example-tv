419e1efc47cb643f37cccea07c60d42ceb219a7d
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-19 13:37:26 -0400

    New signal to handle user registration

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, UserManager, Group
 from django.db.models.signals import post_save
 
-from .signals import create_auth_token
+from .signals import create_auth_token, new_breeder_signal, new_vet_signal
 
 from .mixins import PermissionsMixin
 
@@ -68,6 +68,11 @@ class Breeder(models.Model):
         super(Breeder, self).save(self, *args, **kwargs)
 
 
+post_save.connect(
+    new_breeder_signal, sender=Breeder,
+    dispatch_uid="users.models.breeder_post_save")
+
+
 class Veterinarian(models.Model):
     area_interest = models.CharField(max_length=150)
     veterinary_school = models.CharField(max_length=50)
@@ -84,3 +89,8 @@ class Veterinarian(models.Model):
 
     def __unicode__(self):
         return u'%s %s' % (self.user.full_name, self.veterinarian_type)
+
+
+post_save.connect(
+    new_vet_signal, sender=Veterinarian,
+    dispatch_uid="users.models.veterinarian_post_save")
--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,46 @@
+from django.core.mail import EmailMessage
 from rest_framework.authtoken.models import Token
+from django.conf import settings
 
 
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+def new_breeder_signal(sender, instance=None, created=False, **kwargs):
+    if created:
+        message_title = (getattr(
+            settings, 'EMAIL_SUBJECT_PREFIX', None) +
+            getattr(settings, 'BREADER_MESSAGE_ADMIN_TITLE', None))
+        message_body = 'There is a need breeder registration.'
+        email_from = getattr(settings, 'DEFAULT_FROM_EMAIL', None)
+        admin_email = getattr(settings, 'ADMIN_EMAIL', None)
+        email = EmailMessage(
+            subject=message_title,
+            body=message_body,
+            from_email=email_from,
+            to=[admin_email],
+            reply_to=[email_from],
+        )
+        email.content_subtype = "html"
+        email.send()
+
+
+def new_vet_signal(sender, instance=None, created=False, **kwargs):
+    if created:
+        message_title = (getattr(
+            settings, 'EMAIL_SUBJECT_PREFIX', None) +
+            getattr(settings, 'BREADER_MESSAGE_ADMIN_TITLE', None))
+        message_body = 'There is a need vet registration.'
+        email_from = getattr(settings, 'DEFAULT_FROM_EMAIL', None)
+        admin_email = getattr(settings, 'ADMIN_EMAIL', None)
+        email = EmailMessage(
+            subject=message_title,
+            body=message_body,
+            from_email=email_from,
+            to=[admin_email],
+            reply_to=[email_from],
+        )
+        email.content_subtype = "html"
+        email.send()
