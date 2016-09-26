f4ef6404304297ec8a96c2e778a2d77744095503
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-26 16:25:21 -0400

    New Pet model

--- pets/models.py
+++ pets/models.py
@@ -3,27 +3,53 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.timezone import now
 
+PET_GENDER = (
+    ('male', 'Male'),
+    ('female', 'Female')
+)
+
+
+def get_current_year():
+    '''
+    Get the current year.
+    '''
+    return now().year
+
+
+def get_limit_year():
+    '''
+    Get the current year minus 100.
+    '''
+    return now().year - 100
+
+
+def uploads_path(instance, filename):
+    '''
+    Function to organize the upload directory
+    this way, every file is organized by username and management
+    is a lot faster
+    '''
+    return '/'.join(['uploads', instance.user.username, filename])
+
 
 class Pet(models.Model):
     name = models.CharField(max_length=50)
     fixed = models.BooleanField(default=False)
-    image = models.ImageField()
+    image = models.ImageField(null=True, blank=True, upload_to=uploads_path)
     age = models.IntegerField(
         validators=[
-            MinValueValidator(now().year - 100),
-            MaxValueValidator(now().year)
-        ])
+            MinValueValidator(get_current_year),
+            MaxValueValidator(get_limit_year)
+        ]
+    )  # We just need the year, int is better optimized
+    pet_type = models.CharField(max_length=150)  # Need the list to complete
+    breed = models.CharField(max_length=150)  # Need the list to complete
+    gender = models.CharField(choices=PET_GENDER, max_length=50)
+    user = models.ForeignKey('users.User')  # 20 limit per user
 
     class Meta:
         verbose_name = "Pet"
         verbose_name_plural = "Pets"
 
-    def __str__(self):
-        pass
-
-    def save(self):
-        pass
-
-    @models.permalink
-    def get_absolute_url(self):
-        return ('')
+    def __unicode__(self):
+        return u"%s - %s" % (self.id, self.name)
