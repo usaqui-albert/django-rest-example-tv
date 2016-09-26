206e433cd4c157917e5928b4d10cfe8f8e16ceb9
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-26 19:08:30 -0400

    add and fix test for user and pets

--- pets/tests/test_views.py
+++ pets/tests/test_views.py
@@ -17,11 +17,20 @@ class TestPetListCreateView:
 
     def test_get_request_no_auth(self):
         req = self.factory.get('/')
-        resp = views.UserView.as_view()(req)
+        resp = views.PetsListCreateView.as_view()(req)
         assert resp.status_code == 403, (
             'Should return Method Forbidden (403) with a json ' +
             '"detail": "Authentication credentials were not provided."'
         )
+
+    def test_get_request(self):
+        user = mixer.blend(models.User)
+        req = self.factory.get('/')
+        force_authenticate(req, user=user)
+        resp = views.PetsListCreateView.as_view()(req)
+        assert resp.status_code == 200, (
+            'Should return OK (200) and a json response ' +
+            'with a list of all users.')
 
     def test_post_valid_data(self):
         data = {
@@ -31,7 +40,7 @@ class TestPetListCreateView:
             'username': 'JDoe'
         }
         req = self.factory.post('/', data=data)
-        resp = views.UserView.as_view()(req)
+        resp = views.PetsListCreateView.as_view()(req)
         assert resp.status_code == 201, (
             'Should return Created (201) and a json response with ' +
             'username, token, ful_name, groups, id and email'
@@ -45,17 +54,8 @@ class TestPetListCreateView:
             'username': 'JDoe'
         }
         req = self.factory.post('/', data=data)
-        resp = views.UserView.as_view()(req)
+        resp = views.PetsListCreateView.as_view()(req)
         assert resp.status_code == 400, (
             'Should return Bad Request (400) with an invalid email'
         )
-
-    def test_get_request(self):
-        user = mixer.blend(models.User)
-        req = self.factory.get('/')
-        force_authenticate(req, user=user)
-        resp = views.UserView.as_view()(req)
-        assert resp.status_code == 200, (
-            'Should return OK (200) and a json response ' +
-            'with a list of all users.')
 
