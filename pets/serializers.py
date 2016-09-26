eff9e1bb15b61e16b77bdfdd5f24cddc8fb6a42e
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-26 16:54:39 -0400

    pets serializer

---
+++ pets/serializers.py
@@ -0,0 +1,25 @@
+from django.core.exceptions import ValidationError
+
+from rest_framework import serializers
+
+from .models import Pet
+
+
+class PetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pet
+        fields = (
+            'user', 'name', 'fixed', 'image',
+            'age', 'pet_type', 'breed', 'id', 'gender'
+        )
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'user': {'read_only': True}
+        }
+
+    def create(self, validated_data):
+        if Pet.objects.filter(user=self.context['user']).count() > 19:
+            raise ValidationError('Max three rounds allowed!')
+        pet = Pet(**dict(validated_data, user=self.context['user']))
+        pet.save()
+        return pet
