d2ac1f23b993f2c3cb95627cf2af9539a043feed
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-27 16:16:05 -0400

    New test for model pets

--- pets/tests/test_views.py
+++ pets/tests/test_views.py
@@ -20,8 +20,8 @@ class TestPetListCreateView:
     def test_get_request_no_auth(self):
         req = self.factory.get('/')
         resp = views.PetsListCreateView.as_view()(req)
-        assert resp.status_code == 403, (
-            'Should return Method Forbidden (403) with a json ' +
+        assert resp.status_code == 401, (
+            'Should return Method UNAUTHORIZED (401) with a json ' +
             '"detail": "Authentication credentials were not provided."'
         )
 
@@ -31,32 +31,49 @@ class TestPetListCreateView:
         force_authenticate(req, user=user)
         resp = views.PetsListCreateView.as_view()(req)
         assert resp.status_code == 403, (
+            'Should return Method Forbidden (403) with a json ' +
+            '"detail": "Admin level is needed for this action."')
+
+    def test_get_request_admin(self):
+        user = mixer.blend(User, is_staff=True)
+        req = self.factory.get('/')
+        force_authenticate(req, user=user)
+        resp = views.PetsListCreateView.as_view()(req)
+        assert resp.status_code == 200, (
             'Should return OK (200) and a json response ' +
-            'with a list of all users.')
+            'with a list of all pets.')
 
-    # def test_post_valid_data(self):
-    #     data = {
-    #         'email': 'john_doe@test.com',
-    #         'password': 'a1234567',
-    #         'full_name': 'John Doe',
-    #         'username': 'JDoe'
-    #     }
-    #     req = self.factory.post('/', data=data)
-    #     resp = views.PetsListCreateView.as_view()(req)
-    #     assert resp.status_code == 201, (
-    #         'Should return Created (201) and a json response with ' +
-    #         'username, token, ful_name, groups, id and email'
-    #     )
+    def test_post_valid_data(self):
+        user = mixer.blend(User)
+        data = {
+            'name': 'john doe',
+            'fixed': 'True',
+            'age': '2016',
+            'pet_type': 'dog',
+            'breed': 'Labrator',
+            'gender': 'male',
+        }
+        req = self.factory.post('/', data=data)
+        force_authenticate(req, user=user)
+        resp = views.PetsListCreateView.as_view()(req)
+        assert resp.status_code == 201, (
+            'Should return Created (201) and a json response with ' +
+            'name, fixed, age, pet_type, breed, gender')
 
-    # def test_post_invalid_data(self):
-    #     data = {
-    #         'email': 'john_doe@com',
-    #         'password': 'a1234567',
-    #         'full_name': 'John Doe',
-    #         'username': 'JDoe'
-    #     }
-    #     req = self.factory.post('/', data=data)
-    #     resp = views.PetsListCreateView.as_view()(req)
-    #     assert resp.status_code == 400, (
-    #         'Should return Bad Request (400) with an invalid email'
-    #     )
+    def test_post_invalid_data(self):
+        user = mixer.blend(User)
+        data = {
+            'name': 'john doe',
+            'fixed': 'True',
+            'age': '16',
+            'pet_type': 'dog',
+            'breed': 'Labrator',
+            'gender': 'male',
+        }
+        req = self.factory.post('/', data=data)
+        force_authenticate(req, user=user)
+        resp = views.PetsListCreateView.as_view()(req)
+        assert resp.status_code == 400, (
+            'Should return Bad Request (400) with an error:' +
+            'The pet age cannot be lower than 1916'
+        )
