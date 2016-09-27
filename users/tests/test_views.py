30a23fc8ba18f2d14c0169d2596609d2d72c6cd2
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-27 19:33:00 -0400

    New test, to test uniquest of email

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -116,6 +116,21 @@ class TestUserView:
             'Should return Bad Request (400) with an invalid email'
         )
 
+    def test_post_data_duplicate(self):
+        user = mixer.blend(models.User)
+        data = {
+            'email': user.email,
+            'password': 'a1234567',
+            'full_name': 'John Doe',
+            'username': 'JDoe'
+        }
+        req = self.factory.post('/', data=data)
+        resp = views.UserView.as_view()(req)
+        assert resp.status_code == 400, (
+            'Should return Bad Request (400) user with this email ' +
+            'already exists.'
+        )
+
     def test_get_request(self):
         user = mixer.blend(models.User)
         req = self.factory.get('/')
