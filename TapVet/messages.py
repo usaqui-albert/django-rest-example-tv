1fd3f93437cdc0f6167b12d55540e87903d6c9ae
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-09-30 16:55:45 -0400

    create a messages module to add consistency

---
+++ TapVet/messages.py
@@ -0,0 +1,9 @@
+
+
+pet_no_pets = 'This user doesn\'t have pets.'
+
+admin_delete = 'You need admin status to delete.'
+admin_required = 'Admin level is needed for this action.'
+
+user_login = 'Authentication credentials were not provided.'
+bad_login = 'Your username and password do not match.'
