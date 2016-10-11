bea97063c4827615a5918b7e1203ec9e220332b8
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-10-11 08:33:46 -0400

    New PostVetListCreateView

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import ListCreateAPIView
+
+class PostVetListCreateView(ListCreateAPIView):
+    """
+    Service to create new users.
+    Need authentication to list user
+
+    :accepted methods:
+    GET
+    POST
+    """
+    serializer_class = CreateUserSerializer
+    permission_classes = (permissions.AllowAny,)
+    queryset = User.objects.all()
