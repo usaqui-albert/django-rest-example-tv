59173e3e1d0c3d673779f64a27796191e9fb2528
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-10-11 17:00:19 -0400

    add new fields to display

--- posts/admin.py
+++ posts/admin.py
@@ -7,7 +7,8 @@ class PostAdmin(admin.ModelAdmin):
     '''
         Admin View for Post
     '''
-    list_display = ('id', 'user', 'pet', 'get_likes')
+    list_display = (
+        'id', 'user', 'pet', 'visible_by_vet', 'visible_by_owner', 'get_likes')
     list_filter = ('visible_by_vet', 'visible_by_owner')
     search_fields = ('user', 'pet')
 
