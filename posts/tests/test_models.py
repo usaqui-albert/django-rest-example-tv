fdaacb727645750ee9338e59448fbf49d154733d
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-10-13 18:13:31 -0400

    New tests cases

--- posts/tests/test_models.py
+++ posts/tests/test_models.py
@@ -1,12 +1,17 @@
 """Testing models"""
 
 import pytest
+from django.core.management import call_command
+
 from mixer.backend.django import mixer
+
+from pets.models import get_current_year
 
 pytestmark = pytest.mark.django_db
 
 
 class TestPost:
+
     def test_model(self):
         obj = mixer.blend('posts.Post')
         assert obj.pk == 1, 'Should create a Post instance'
@@ -33,3 +38,56 @@ class TestPost:
         assert not obj.is_paid(), 'Should return False'
         obj.set_paid()
         assert obj.is_paid(), 'Should return True'
+
+    def test_post_if_vet_no_verified(self):
+        call_command(
+            'loaddata', '../../users/fixtures/users.json', verbosity=0)
+        user = mixer.blend('users.user', groups_id=3)
+        obj = mixer.blend('posts.post', user=user)
+        assert not obj.visible_by_vet
+        assert not obj.visible_by_owner
+
+    def test_post_if_vet_verified(self):
+        call_command(
+            'loaddata', '../../users/fixtures/users.json', verbosity=0)
+        call_command(
+            'loaddata', '../../countries/fixtures/countries.json', verbosity=0)
+        user = mixer.blend('users.user', groups_id=3)
+        mixer.blend(
+            'users.veterinarian', user=user, verified=True,
+            graduating_year=get_current_year() - 11,
+            country_id=1, state_id=2)
+        obj = mixer.blend('posts.post', user=user)
+        assert obj.visible_by_vet
+        assert not obj.visible_by_owner
+
+    def test_post_if_student(self):
+        call_command(
+            'loaddata', '../../users/fixtures/users.json', verbosity=0)
+        call_command(
+            'loaddata', '../../countries/fixtures/countries.json', verbosity=0)
+        user = mixer.blend('users.user', groups_id=3)
+        mixer.blend(
+            'users.veterinarian', user=user, verified=True,
+            graduating_year=get_current_year() - 11,
+            country_id=1, state_id=2)
+        obj = mixer.blend('posts.post', user=user)
+        assert obj.visible_by_vet
+        assert not obj.visible_by_owner
+
+    def test_post_if_pet_owner(self):
+        call_command(
+            'loaddata', '../../users/fixtures/users.json', verbosity=0)
+        user = mixer.blend('users.user', groups_id=1)
+        obj = mixer.blend('posts.post', user=user)
+        assert not obj.visible_by_vet
+        assert obj.visible_by_owner
+
+    def test_post_if_pet_owner_and_paid(self):
+        call_command(
+            'loaddata', '../../users/fixtures/users.json', verbosity=0)
+        user = mixer.blend('users.user', groups_id=1)
+        obj = mixer.blend('posts.post', user=user)
+        obj.set_paid()
+        assert obj.visible_by_vet
+        assert obj.visible_by_owner
