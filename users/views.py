51890d6f0ba02bca8693f9cbb1f4e7d1f180d549
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-10-25 09:39:39 -0400

    remove space before :

--- users/views.py
+++ users/views.py
@@ -52,7 +52,7 @@ class UserAuth(ObtainAuthToken):
                 'id': user.id,
                 'full_name': user.full_name,
                 'email': user.email,
-                'groups' : user.groups.id
+                'groups': user.groups.id
             }
         )
 
