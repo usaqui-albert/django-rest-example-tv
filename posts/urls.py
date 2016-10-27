107b04656ad3c7b91f3165e1c4b5caa5e4ec1095
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-10-27 10:59:16 -0400

    add post detail/update/delete url

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
 from .views import (
-    PostListCreateView, PaidPostView
+    PostListCreateView, PaidPostView, PostRetriveUpdateDeleteView
 )
 
 urlpatterns = [
     url(r'^$', PostListCreateView.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', PostRetriveUpdateDeleteView.as_view()),
     url(r'^(?P<pk>[0-9]+)/paid/$', PaidPostView.as_view()),
 ]
