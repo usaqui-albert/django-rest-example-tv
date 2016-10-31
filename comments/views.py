3d97ea98b5b8b93aa04e0e0897d3e400c5586c71
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-10-31 17:11:30 -0400

    comment vote

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,12 @@
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
+
 from rest_framework.generics import ListCreateAPIView
-from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+
+from posts.models import Post
 
 from .models import Comment
 from .serializers import CommentSerializer
@@ -23,6 +29,15 @@ class CommentsPetOwnerListCreateView(ListCreateAPIView):
         ).annotate(upvoters_count=Count('upvoters'))
         return qs
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        serializer.save(post=post)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class CommentsVetListCreateView(ListCreateAPIView):
     """
@@ -40,3 +55,24 @@ class CommentsVetListCreateView(ListCreateAPIView):
             post_id=self.kwargs['pk'], user__groups_id__in=[3, 4, 5]
         ).annotate(upvoters_count=Count('upvoters'))
         return qs
+
+
+class CommentVoteView(APIView):
+    allowed_methods = ('POST', 'DELETE')
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        comment.upvoters.add(request.user.id)
+        response = {
+            'detail': 'vote added'
+        }
+        return Response(response, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        comment.upvoters.remove(request.user.id)
+        response = {
+            'detail': 'vote removed'
+        }
+        return Response(response, status=status.HTTP_200_OK)
