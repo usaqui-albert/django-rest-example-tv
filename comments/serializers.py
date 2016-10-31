2dff9b693c41432fc2c372950d8b4216828219e1
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-10-31 15:28:51 -0400

    add upvoter_count field

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,11 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, IntegerField
 
 from .models import Comment
 
 
 class CommentSerializer(ModelSerializer):
+    upvoters_count = IntegerField(read_only=True)
 
     class Meta:
         model = Comment
         fields = (
-            'description', 'id', 'user', 'post', 'created_at', 'updated_at')
+            'description', 'id', 'user', 'post', 'created_at', 'updated_at',
+            'upvoters_count')
