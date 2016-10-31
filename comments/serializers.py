efcd7e258584e07a0e683d686b8f3abc4dc634a5
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-10-31 18:49:57 -0400

    crete def and extra kwargs.
    user: {'read_only': True}, 
    post: {'read_only': True},

--- comments/serializers.py
+++ comments/serializers.py
@@ -11,3 +11,12 @@ class CommentSerializer(ModelSerializer):
         fields = (
             'description', 'id', 'user', 'post', 'created_at', 'updated_at',
             'upvoters_count')
+        extra_kwargs = {
+            'user': {'read_only': True},
+            'post': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        comment = Comment(**dict(validated_data, user=self.context['user']))
+        comment.save()
+        return comment
