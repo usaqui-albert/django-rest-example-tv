4a768256839497045c4cd4c4e38a1d5d5e5f9f06
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-10-31 15:12:50 -0400

    CommentsListCreateView

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from posts.models import Post
+
+from .models import Comment
+
+
+class CommentsListCreateView(ListCreateAPIView):
+    """
+    Service to create list and create new comments for post.
+
+    :accepted methods:
+    GET
+    POST
+    """
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    def get_queryset(self):
+        qs = Comment.objects.filter(
+            post_id=self.kwargs['pk']
+        ).annotate(upvoters_count=Count('upvoters'))
+        return qs
