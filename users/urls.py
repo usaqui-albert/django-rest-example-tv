609e18568ddcf825e5ad30e8bba67fa42ae83de7
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-11-01 12:09:20 -0400

    add posts/<id>/like/ to like a post

--- users/urls.py
+++ users/urls.py
@@ -7,12 +7,13 @@ from .views import (
     UserAuth, UserView, VeterinarianListCreateView,
     BreederListCreateView, GroupsListView,
     AuthorizeBreederView, AuthorizeVetView, AreaInterestListView,
-    UserRetrieveUpdateView, StripeCustomerView
+    UserRetrieveUpdateView, StripeCustomerView, PostVoteView
 )
 
 urlpatterns = [
     url(r'^$', UserView.as_view()),
     url(r'^(?P<pk>\d+)/$', UserRetrieveUpdateView.as_view()),
+    url(r'^(?P<pk>\d+)/like/$', PostVoteView.as_view()),
     url(r'^(?P<pk>\d+)/pets/$', PetListByUser.as_view()),
     url(r'^(?P<pk>\d+)/payments/$', StripeCustomerView.as_view()),
     url(r'^(?P<pk>\d+)/posts/$', PostByUserListView.as_view()),
