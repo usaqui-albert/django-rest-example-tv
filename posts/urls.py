f223b0a0b2d63e0f89faf926fd63c6c444a22b6d
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-11-01 13:55:51 -0400

    pluralize url name

--- posts/urls.py
+++ posts/urls.py
@@ -12,9 +12,11 @@ urlpatterns = [
     url(r'^$', PostListCreateView.as_view()),
     url(r'^(?P<pk>[0-9]+)/$', PostRetriveUpdateDeleteView.as_view()),
     url(r'^(?P<pk>[0-9]+)/paid/$', PaidPostView.as_view()),
-    url(r'^(?P<pk>[0-9]+)/vet-comment/$', CommentsVetListCreateView.as_view()),
     url(
-        r'^(?P<pk>[0-9]+)/pet-comment/$',
+        r'^(?P<pk>[0-9]+)/vet-comments/$',
+        CommentsVetListCreateView.as_view()),
+    url(
+        r'^(?P<pk>[0-9]+)/pet-comments/$',
         CommentsPetOwnerListCreateView.as_view()
     ),
     url(r'^images/(?P<pk>[0-9]+)/$', ImagePostDeleteView.as_view()),
