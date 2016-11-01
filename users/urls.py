b8388e775203de3a924315bf8d873d8fa7f67587
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-11-01 16:44:25 -0400

    remove unused lines

--- users/urls.py
+++ users/urls.py
@@ -7,13 +7,12 @@ from .views import (
     UserAuth, UserView, VeterinarianListCreateView,
     BreederListCreateView, GroupsListView,
     AuthorizeBreederView, AuthorizeVetView, AreaInterestListView,
-    UserRetrieveUpdateView, StripeCustomerView, PostVoteView
+    UserRetrieveUpdateView, StripeCustomerView
 )
 
 urlpatterns = [
     url(r'^$', UserView.as_view()),
     url(r'^(?P<pk>\d+)/$', UserRetrieveUpdateView.as_view()),
-    url(r'^(?P<pk>\d+)/like/$', PostVoteView.as_view()),
     url(r'^(?P<pk>\d+)/pets/$', PetListByUser.as_view()),
     url(r'^(?P<pk>\d+)/payments/$', StripeCustomerView.as_view()),
     url(r'^(?P<pk>\d+)/posts/$', PostByUserListView.as_view()),
