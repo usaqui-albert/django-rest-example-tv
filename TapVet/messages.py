07838df2ce428556f48e8562025538ea036afa82
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-11-03 09:46:41 -0400

    change the for double quotes and add points to end of sentences

--- TapVet/messages.py
+++ TapVet/messages.py
@@ -1,13 +1,13 @@
 
 
-pet_no_pets = 'This user doesn\'t have pets.'
+pet_no_pets = "This user doesn't have pets."
 
-admin_delete = 'You need admin status to delete.'
-admin_required = 'Admin level is needed for this action.'
+admin_delete = "You need admin status to delete."
+admin_required = "Admin level is needed for this action."
 
-user_login = 'Authentication credentials were not provided.'
-bad_login = 'Your username and password do not match.'
+user_login = "Authentication credentials were not provided."
+bad_login = "Your username and password do not match."
 
-permission = 'Error: You dont have permission to edit'
-one_image = 'Error: A post need at least one image'
-comment_permission = {'detail': 'Error: You dont have permission to comment'}
+permission = "Error: You don't have permission to edit."
+one_image = "Error: A post need at least one image."
+comment_permission = {"detail": "Error: You don't have permission to comment."}
