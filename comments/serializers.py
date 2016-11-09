1f7146cc3a9a82f0eb18bdac47677086155faea8
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-11-08 20:19:10 -0400

    add full name to each comment

--- comments/serializers.py
+++ comments/serializers.py
@@ -6,12 +6,13 @@ from .models import Comment
 class CommentSerializer(ModelSerializer):
     upvoters_count = IntegerField(read_only=True)
     label = CharField(source='user.groups.name', read_only=True)
+    full_name = CharField(source='user.full_name', read_only=True)
 
     class Meta:
         model = Comment
         fields = (
             'description', 'id', 'user', 'post', 'created_at', 'updated_at',
-            'upvoters_count', 'label')
+            'upvoters_count', 'label', 'full_name')
         extra_kwargs = {
             'user': {'read_only': True},
             'post': {'read_only': True}
