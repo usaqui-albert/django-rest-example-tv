4fdd4443b55513848269f1e21fdbf35e208bcdbf
Albert Usaqui <albert.usaqui@blanclink.com>
2016-11-24 10:39:39 -0400

    adding new method to the CustomTestCase class to load the FeedVariable fixtures for unit testing

--- helpers/tests_helpers.py
+++ helpers/tests_helpers.py
@@ -19,6 +19,11 @@ class CustomTestCase:
             'loaddata', '../../countries/fixtures/countries.json', verbosity=0)
         return self
 
+    def load_feed_variables(self):
+        call_command(
+            'loaddata', '../../posts/fixtures/posts.json', verbosity=0)
+        return self
+
     @staticmethod
     def get_user(**kwargs):
         return mixer.blend('users.User', **kwargs)
