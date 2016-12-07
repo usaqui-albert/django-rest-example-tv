60b2c3af09742c5e945513f7564b9a66a8eede86
Leopoldo Pimentel <leopoldop@blanclabs.com>
2016-12-07 13:32:20 -0400

    new view to list users in dashboard

--- users/dashboard/views.py
+++ users/dashboard/views.py
@@ -1,12 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAdminUser
 from rest_framework import status
 
 from TapVet import messages
+from TapVet.pagination import StandardPagination
 from users.serializers import UserLoginSerializer
+from users.models import User
 
-from .serializers import AdminAuthTokenSerializer
+from .serializers import AdminAuthTokenSerializer, AdminUserSerializer
 
 
 class AdminAuth(ObtainAuthToken):
@@ -33,3 +37,10 @@ class AdminAuth(ObtainAuthToken):
                 messages.inactive, status=status.HTTP_403_FORBIDDEN)
         serializer = UserLoginSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class AdminUsersListView(ListAPIView):
+    pagination_class = StandardPagination
+    queryset = User.objects.all()
+    permission_classes = (IsAdminUser,)
+    serializer_class = AdminUserSerializer
