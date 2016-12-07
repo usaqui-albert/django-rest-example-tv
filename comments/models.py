02f1dc6257ddd4c3a3e5ca74406557d6f5ddd897
Leopoldo Pimentel <leopoldop@blanclabs.com>
2016-12-07 14:21:06 -0400

    TAPVET-501 #comment Create model #start-progress

--- comments/models.py
+++ comments/models.py
@@ -14,3 +14,17 @@ class Comment(models.Model):
 
     def __unicode__(self):
         return u'comment: %s - created: %s' % (self.id, self.created_at)
+
+
+class Feedback(models.Model):
+    post = models.ForeignKey('posts.post')
+    user = models.ForeignKey('users.user')
+    was_helpful = models.BooleanField()
+    descritpion = models.CharField(max_length=500, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.post.user == self.user:
+            raise ValueError(
+                "The feedback is only valid if is been done by the post owner"
+            )
+        super(Feedback, self).save(*args, **kwargs)
