1f9c10461cc516f756379e445969e0020ab756c1
Albert Usaqui <albert.usaqui@blanclink.com>
2016-12-10 12:32:11 -0400

    adding some messages and some stripe helpers

--- TapVet/messages.py
+++ TapVet/messages.py
@@ -27,3 +27,9 @@ inactive = {
 already_liked = {
     "detail": "This post is already liked by you."
 }
+forbidden_action = {
+    "detail": "You are not allowed to do this action."
+}
+card_updated_successfully = {
+    "detail": "Your card has been updated successfully."
+}
