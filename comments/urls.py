b4a675c9cbb17e64a9b6fbd34d9c9da5314b6307
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2016-12-12 15:09:01 -0400

    TAPVET-503 #comment rename the url

--- comments/urls.py
+++ comments/urls.py
@@ -5,5 +5,5 @@ from .views import CommentVoteView, FeedbackCreateView
 
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/vote/$', CommentVoteView.as_view()),
-    url(r'^(?P<pk>[0-9]+)/comment/$', FeedbackCreateView.as_view()),
+    url(r'^(?P<pk>[0-9]+)/feedbacks/$', FeedbackCreateView.as_view()),
 ]
