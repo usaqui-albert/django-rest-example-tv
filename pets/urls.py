387a89ade777f7682843989193d2925ab6927e64
Albert Usaqui <albert.usaqui@blanclink.com>
2016-12-14 10:10:05 -0400

    PetCreateView instead PetsListCreateView

--- pets/urls.py
+++ pets/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 
 from .views import (
-    PetsListCreateView, PetRetrieveUpdateDeleteView,
-    PetTypeListView
+    PetCreateView, PetRetrieveUpdateDeleteView, PetTypeListView
 )
 
 urlpatterns = [
-    url(r'^$', PetsListCreateView.as_view()),
+    url(r'^$', PetCreateView.as_view()),
     url(r'^(?P<pk>\d+)/$', PetRetrieveUpdateDeleteView.as_view()),
     url(r'^pet-types/$', PetTypeListView.as_view()),
 ]
