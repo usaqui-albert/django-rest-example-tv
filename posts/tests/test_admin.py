b9ce2db30f5a4c210090562715d90a0c61afd06d
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2017-01-09 18:22:44 -0400

    TAPVET-673 #comment test cases

--- posts/tests/test_admin.py
+++ posts/tests/test_admin.py
@@ -10,15 +10,15 @@ from posts.dashboard import views
 pytestmark = pytest.mark.django_db
 
 
-class AdminActiveDeactivePostView(CustomTestCase):
-
-    def test_put_request_not_allowed(self):
-        req = self.factory.put('/', data={})
-        resp = views.AdminActiveDeactivePostView.as_view()(req)
-        assert resp.status_code == 401
+class TestAdminActiveDeactivePostView(CustomTestCase):
 
     def test_get_request_not_allowed(self):
         req = self.factory.get('/', data={})
+        resp = views.AdminActiveDeactivePostView.as_view()(req)
+        assert resp.status_code == 401
+
+    def test_put_request_not_allowed(self):
+        req = self.factory.put('/', data={})
         resp = views.AdminActiveDeactivePostView.as_view()(req)
         assert resp.status_code == 401
 
@@ -34,27 +34,31 @@ class AdminActiveDeactivePostView(CustomTestCase):
         resp = views.AdminActiveDeactivePostView.as_view()(req)
         assert resp.status_code == 405
 
-    # def test_get_request_no_auth(self):
-        # post = mixer.blend('posts.post')
-    #     req = self.factory.get('/')
-    #     awaited_message = 'Authentication credentials were not provided.'
-    #     resp = views.AdminActiveDeactivePostView.as_view()(req)
-    #     assert 'detail' in resp.data
-    #     assert resp.data['detail'] == awaited_message
-    #     assert resp.status_code == 401
+    def test_patch_request_not_admin(self):
+        user = self.load_users_data().get_user(is_staff=False)
+        req = self.factory.get('/', data={})
+        force_authenticate(req, user=user)
+        resp = views.AdminActiveDeactivePostView.as_view()(req)
+        assert resp.status_code == 403
 
-    # def test_get_request_no_admin(self):
-    #     user = self.load_users_data().get_user()
-    #     req = self.factory.get('/')
-    #     force_authenticate(req, user=user)
-    #     resp = views.AdminActiveDeactivePostView.as_view()(req)
-    #     assert 'detail' in resp.data
-    #     assert resp.status_code == 403
+    def test_patch_request_admin_active_false(self):
+        user = self.load_users_data().get_user(is_staff=True)
+        post = mixer.blend('posts.post', active=True)
+        req = self.factory.patch('/', data={'active': False})
+        force_authenticate(req, user=user)
+        resp = views.AdminActiveDeactivePostView.as_view()(req, pk=post.pk)
+        assert resp.status_code == 200
+        assert not resp.data['active']
+        post.refresh_from_db()
+        assert not post.active
 
-    # def test_get_request_admin(self):
-    #     user = self.load_users_data().get_user(is_staff=True)
-    #     req = self.factory.get('/')
-    #     force_authenticate(req, user=user)
-    #     resp = views.AdminActiveDeactivePostView.as_view()(req)
-    #     assert 'results' in resp.data
-    #     assert resp.status_code == 200
+    def test_patch_request_admin_active_true(self):
+        user = self.load_users_data().get_user(is_staff=True)
+        post = mixer.blend('posts.post', active=False)
+        req = self.factory.patch('/', data={'active': True})
+        force_authenticate(req, user=user)
+        resp = views.AdminActiveDeactivePostView.as_view()(req, pk=post.pk)
+        assert resp.status_code == 200
+        assert resp.data['active']
+        post.refresh_from_db()
+        assert post.active
