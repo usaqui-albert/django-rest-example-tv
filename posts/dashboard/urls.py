e5da7bc0e3c999bee5af9455ec390b01c0fa69f3
Albert Usaqui <albert.usaqui@blanclink.com>
2017-01-12 12:55:36 -0400

    service to allow an admin delete a comment

--- posts/dashboard/urls.py
+++ posts/dashboard/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from .views import (
     AdminPostView, AdminPostDetailView
 )
-from comments.dashboard.views import PetOwnerCommentsView, VetCommentsView
+from comments.dashboard.views import (
+    PetOwnerCommentsView, VetCommentsView, AdminCommentDetailView
+)
 
 
 urlpatterns = [
@@ -11,5 +13,9 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', AdminPostDetailView.as_view()),
     url(r'^(?P<pk>[0-9]+)/pet-comments/$', PetOwnerCommentsView.as_view()),
     url(r'^(?P<pk>[0-9]+)/vet-comments/$', VetCommentsView.as_view()),
+    url(
+        r'^(?P<pk_post>[0-9]+)/comments/(?P<pk_comment>[0-9]+)/$',
+        AdminCommentDetailView.as_view()
+    ),
 
 ]
