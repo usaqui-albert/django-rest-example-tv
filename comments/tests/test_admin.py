ce45c97084bac298966093c6dfaacb0fa70e1297
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2017-01-18 18:17:20 -0400

    tests for service

--- comments/tests/test_admin.py
+++ comments/tests/test_admin.py
@@ -3,6 +3,8 @@ import pytest
 
 from rest_framework.test import force_authenticate
 
+from mixer.backend.django import mixer
+
 from helpers.tests_helpers import CustomTestCase
 
 from comments.dashboard import views
@@ -44,4 +46,45 @@ class TestAdminFeedbackListView(CustomTestCase):
         req = self.factory.get('/')
         force_authenticate(req, user=self.get_user(is_staff=True))
         resp = views.AdminFeedbackListView.as_view()(req)
+        assert resp.status_code == 200
+
+
+class TestAdminFeedbackView(CustomTestCase):
+
+    def test_put_request_admin_not_allowed(self):
+        req = self.factory.put('/', {})
+        force_authenticate(req, user=self.get_user(is_staff=True))
+        resp = views.AdminFeedbackView.as_view()(req)
+        assert 'detail' in resp.data
+        assert resp.status_code == 405
+
+    def test_put_request_not_allowed(self):
+        req = self.factory.put('/', {})
+        force_authenticate(req, user=self.get_user())
+        resp = views.AdminFeedbackView.as_view()(req)
+        assert 'detail' in resp.data
+        assert resp.status_code == 403
+
+    def test_post_request_not_allowed(self):
+        req = self.factory.post('/', {})
+        force_authenticate(req, user=self.get_user())
+        resp = views.AdminFeedbackView.as_view()(req)
+        assert 'detail' in resp.data
+        assert resp.status_code == 403
+
+    def test_get_not_allowed(self):
+        req = self.factory.get('/')
+        force_authenticate(req, user=self.get_user())
+        resp = views.AdminFeedbackView.as_view()(req)
+        assert 'detail' in resp.data
+        assert resp.status_code == 403
+
+    def test_get_allowed(self):
+        req = self.factory.get('/')
+        user = self.load_users_data().get_user(groups_id=1)
+        post = mixer.blend('posts.post', user=user)
+        comment = mixer.blend('comments.comment', post=post)
+        feedback = mixer.blend('comments.feedback', comment=comment, user=user)
+        force_authenticate(req, user=self.get_user(is_staff=True))
+        resp = views.AdminFeedbackView.as_view()(req, pk=feedback.pk)
         assert resp.status_code == 200
