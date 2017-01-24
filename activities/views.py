34a86efe3bc8ccbd6d28d80d90e9d5fd5d3b6c2d
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2017-01-24 11:33:40 -0400

    add user variable,, add pagination

--- activities/views.py
+++ activities/views.py
@@ -1,6 +1,9 @@
 from django.db.models import Q
 
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from TapVet.pagination import StandardPagination
 
 from .models import Activity
 from .serializers import ActivitySerializer
@@ -9,26 +12,34 @@ from .serializers import ActivitySerializer
 class UserLikedPostListView(ListAPIView):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
+    permission_classes = (IsAuthenticated,)
+    pagination_class = StandardPagination
 
     def get_queryset(self):
         qs = self.queryset
-        qs = qs.filter(user=self.request.user, action=Activity.LIKE)
+        user = self.request.user
+        qs = qs.filter(user=user, action=Activity.LIKE)
         return qs.all()
 
 
 class UserCommentPostListView(ListAPIView):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
+    permission_classes = (IsAuthenticated,)
+    pagination_class = StandardPagination
 
     def get_queryset(self):
         qs = self.queryset
-        qs = qs.filter(user=self.request.user, action=Activity.COMMENT)
+        user = self.request.user
+        qs = qs.filter(user=user, action=Activity.COMMENT)
         return qs.all()
 
 
 class ActivityListView(ListAPIView):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
+    permission_classes = (IsAuthenticated,)
+    pagination_class = StandardPagination
 
     def get_queryset(self):
         user = self.request.user
@@ -38,10 +49,13 @@ class ActivityListView(ListAPIView):
                 post__user=user, action=Activity.LIKE
             ) |
             Q(
-                comment__user=user, action=Activity.UPVOTE  #c
+                comment__user=user, action=Activity.UPVOTE
             ) |
             Q(
-                post__user=user, action=Activity.COMMENT  #a
+                post__user=user, action=Activity.COMMENT
+            ) |
+            Q(
+                post__in=user.likes.all(), action=Activity.COMMENT
             )
-        )
+        ).exclude(user=user)
         return qs.all()
