6db67052d433cffcb9aaabecca17c08e679a561d
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2017-01-24 11:33:17 -0400

    usign nested serializers

--- activities/serializers.py
+++ activities/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 
+from posts.serializers import PostSmallSerializer
+from users.serializers import UserSerializers
+from comments.serializers import CommentSerializer
 from .models import Activity
 
 
 class ActivitySerializer(ModelSerializer):
+    post = PostSmallSerializer()
+    user = UserSerializers()
+    comment = CommentSerializer()
+    follows = UserSerializers()
 
     class Meta:
         model = Activity
