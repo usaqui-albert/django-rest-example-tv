1814b4eac168c93150edb7ad50971b98cb345f35
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2017-01-26 19:51:14 -0400

    new mail tasks

--- users/tasks.py
+++ users/tasks.py
@@ -1,5 +1,3 @@
-import os
-
 from django.core.mail import mail_admins
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
@@ -10,8 +8,6 @@ from celery.task import task
 
 import sendgrid
 from sendgrid.helpers.mail import (
-    Attachment,
-    Category,
     Content,
     Email,
     Mail,
@@ -41,10 +37,11 @@ def refer_a_friend_by_email(receiver, sender_user):
     msg.send()
 
 
-def welcome_mail(user):
+@task(ignore_result=True)
+def welcome_mail(user, group=None):
     sg = sendgrid.SendGridAPIClient(apikey=settings.SENDGRID_API_KEY)
     mail = Mail()
-    mail.set_template_id(settings.SENDGRID_WELCOME_TEMPLATE)
+    mail.set_template_id(settings.SENDGRID_WELCOME.get(group))
     mail.set_from(Email(settings.DEFAULT_FROM_EMAIL, 'Tapvet Team'))
     personalization = Personalization()
     personalization.add_to(Email(user.email))
@@ -56,5 +53,53 @@ def welcome_mail(user):
     personalization.add_substitution(Substitution('-username-', user.username))
     personalization.add_substitution(Substitution('-email-', user.email))
     mail.add_personalization(personalization)
-    response = sg.client.mail.send.post(request_body=mail.get())
-    print(response.status_code)
+    sg.client.mail.send.post(request_body=mail.get())
+
+
+@task(ignore_result=True)
+def password_reset(user, verification_code):
+    sg = sendgrid.SendGridAPIClient(apikey=settings.SENDGRID_API_KEY)
+    mail = Mail()
+    mail.set_template_id(settings.SENDGRID_PASSWORD_RESET)
+    mail.set_from(Email(settings.DEFAULT_FROM_EMAIL, 'Tapvet Team'))
+    personalization = Personalization()
+    personalization.add_to(Email(user.email))
+    personalization.set_subject(
+        settings.EMAIL_SUBJECT_PREFIX + ' Password Reset -username-')
+    mail.add_content(Content("text/plain", 'hello'))
+    mail.add_content(Content("text/html", 'hello'))
+    personalization.add_substitution(Substitution('-name-', user.full_name))
+    personalization.add_substitution(Substitution('-code-', verification_code))
+    personalization.add_substitution(Substitution('-username-', user.username))
+    personalization.add_substitution(Substitution('-email-', user.email))
+    mail.add_personalization(personalization)
+    sg.client.mail.send.post(request_body=mail.get())
+
+
+@task(ignore_result=True)
+def vet_verify_mail(user, veterinarian_type):
+    sg = sendgrid.SendGridAPIClient(apikey=settings.SENDGRID_API_KEY)
+    mail = Mail()
+
+    if veterinarian_type == 3:
+        mail.set_template_id(
+            settings.SENDGRID_VALIDATED.get('VETERINARIAN')
+        )
+
+    else:
+        mail.set_template_id(
+            settings.SENDGRID_VALIDATED.get('TECHNICIAN_STUDENT')
+        )
+
+    mail.set_from(Email(settings.DEFAULT_FROM_EMAIL, 'Tapvet Team'))
+    personalization = Personalization()
+    personalization.add_to(Email(user.email))
+    personalization.set_subject(
+        settings.EMAIL_SUBJECT_PREFIX + ' Password Reset -username-')
+    mail.add_content(Content("text/plain", 'hello'))
+    mail.add_content(Content("text/html", 'hello'))
+    personalization.add_substitution(Substitution('-name-', user.full_name))
+    personalization.add_substitution(Substitution('-username-', user.username))
+    personalization.add_substitution(Substitution('-email-', user.email))
+    mail.add_personalization(personalization)
+    sg.client.mail.send.post(request_body=mail.get())
