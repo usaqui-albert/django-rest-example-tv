ebb66b64688252c97cbb48754fb6ae8b5568c6db
Leopoldo Pimentel <leopoldo.pimentel@blanclink.com>
2017-01-27 13:25:07 -0400

    new task

--- users/tasks.py
+++ users/tasks.py
@@ -1,7 +1,3 @@
-from django.core.mail import mail_admins
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import get_template
-from django.template import Context
 from django.conf import settings
 
 from celery.task import task
@@ -16,24 +12,14 @@ from sendgrid.helpers.mail import (
 
 
 @task(ignore_result=True)
-def send_mail(subject, message, html_message, fail_silently=True):
-    mail_admins(
-        subject=subject, message=message, fail_silently=fail_silently,
-        html_message=html_message
+def refer_a_friend_by_email(receiver_email, sender_user):
+    mail = [Email(receiver_email)]
+    sendgrid_api(
+        obtain_mail(
+            personalization=obtain_personalization(sender_user, to_mails=mail),
+            template=settings.SENDGRID_REFER_FRIEND
+        )
     )
-
-
-@task(ignore_result=True)
-def refer_a_friend_by_email(receiver, sender_user):
-    subject = 'Awesome! %s has invited you to check out TapVet' % sender_user
-    from_email = settings.EMAIL_HOST_USER
-    to = receiver
-    htmly = get_template('users/partials/email/email_to_refer.html')
-    d = Context({'full_name': sender_user})
-    html_content = htmly.render(d)
-    msg = EmailMultiAlternatives(subject, '', from_email, [to])
-    msg.attach_alternative(html_content, "text/html")
-    msg.send()
 
 
 @task(ignore_result=True)
@@ -42,7 +28,8 @@ def welcome_mail(user, group=None):
         obtain_mail(
             personalization=obtain_personalization(user),
             template=settings.SENDGRID_WELCOME.get(group)
-        ))
+        )
+    )
 
 
 @task(ignore_result=True)
@@ -52,7 +39,8 @@ def password_reset(user, verification_code):
         obtain_mail(
             personalization=obtain_personalization(user, substitutions),
             template=settings.SENDGRID_PASSWORD_RESET
-        ))
+        )
+    )
 
 
 @task(ignore_result=True)
@@ -66,18 +54,57 @@ def vet_verify_mail(user, veterinarian_type):
         obtain_mail(
             personalization=obtain_personalization(user),
             template=template
-        ))
+        )
+    )
 
 
-def obtain_personalization(user, substitutions=None):
+@task(ignore_result=True)
+def send_feedback(user, message):
+    substitutions = [Substitution('-message-', message)]
+    sendgrid_api(
+        obtain_mail(
+            personalization=obtain_personalization(
+                user, substitutions, to_mail=settings.ADMINS[0][1]),
+            template=settings.SENDGRID_FEEDBACK
+        )
+    )
+
+
+@task(ignore_result=True)
+def send_report(user, report):
+    substitutions = [
+        Substitution('-post-', report.post.id),
+        Substitution('-type-', report.get_type_display()),
+        Substitution('-post_message-', report.post.description)
+    ]
+    sendgrid_api(
+        obtain_mail(
+            personalization=obtain_personalization(
+                user, substitutions, to_mail=settings.ADMINS[0][1]),
+            template=settings.SENDGRID_REPORT
+        )
+    )
+
+
+def obtain_personalization(
+    user, substitutions=None, to_mails=None, to_mail=None
+):
     personalization = Personalization()
-    personalization.add_to(Email(user.email))
+
+    if to_mail:
+        personalization.add_to(Email(to_mail))
+    else:
+        personalization.add_to(Email(user.email))
+
     personalization.add_substitution(Substitution('-name-', user.full_name))
     personalization.add_substitution(Substitution('-username-', user.username))
     personalization.add_substitution(Substitution('-email-', user.email))
     if substitutions:
         for substitution in substitutions:
             personalization.add_substitution(substitution)
+    if to_mails:
+        for mail in to_mails:
+            personalization.add_to(mail)
     return personalization
 
 
