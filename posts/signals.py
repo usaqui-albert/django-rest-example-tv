dd2d96d173e1be0967d32655050442baf9f3b3e6
Albert Usaqui <albertu@blanclabs.com>
2017-02-06 15:43:09 -0400

    using the helper function to send push notification messages

--- posts/signals.py
+++ posts/signals.py
@@ -1,8 +1,7 @@
-from push_notifications.models import APNSDevice, GCMDevice
-
 from users.tasks import send_report
 from activities.models import Activity
 from TapVet.messages import liking_post
+from TapVet.utils import send_notification_message
 
 
 def post_reporting_signal(sender, instance=None, created=False, **kwargs):
@@ -13,13 +12,7 @@ def post_reporting_signal(sender, instance=None, created=False, **kwargs):
 def new_post_like_signal(sender, instance=None, created=False, **kwargs):
     if created:
         post_owner_id = instance.post.user_id
-        gcm_device = GCMDevice.objects.filter(user=post_owner_id).first()
-        apns_device = APNSDevice.objects.filter(user=post_owner_id).first()
-
-        if gcm_device:
-            gcm_device.send_message(liking_post)
-        if apns_device:
-            apns_device.send_message(liking_post)
+        send_notification_message(post_owner_id, liking_post)
 
         activity = Activity(
             user=instance.user,
