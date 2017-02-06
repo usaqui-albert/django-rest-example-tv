a5923b567c432eb101b55e5cd5d0049cb1e39eb9
Albert Usaqui <albertu@blanclabs.com>
2017-02-06 11:55:21 -0400

    sending push notifications in the new post like signal function

--- posts/signals.py
+++ posts/signals.py
@@ -1,5 +1,8 @@
+from push_notifications.models import APNSDevice, GCMDevice
+
 from users.tasks import send_report
 from activities.models import Activity
+from TapVet.messages import liking_post
 
 
 def post_reporting_signal(sender, instance=None, created=False, **kwargs):
@@ -9,6 +12,15 @@ def post_reporting_signal(sender, instance=None, created=False, **kwargs):
 
 def new_post_like_signal(sender, instance=None, created=False, **kwargs):
     if created:
+        post_owner_id = instance.post.user_id
+        gcm_device = GCMDevice.objects.filter(user=post_owner_id).first()
+        apns_device = APNSDevice.objects.filter(user=post_owner_id).first()
+
+        if gcm_device:
+            gcm_device.send_message(liking_post)
+        if apns_device:
+            apns_device.send_message(liking_post)
+
         activity = Activity(
             user=instance.user,
             action=Activity.LIKE,
