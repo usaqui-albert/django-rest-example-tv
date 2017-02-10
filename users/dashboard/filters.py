d40914f0e07b0e99b3d6c41cabec65103f290bab
Leopoldo Pimentel <leopoldop@blanclabs.com>
2017-02-10 15:03:12 -0400

    new filter for dasboard users

---
+++ users/dashboard/filters.py
@@ -0,0 +1,15 @@
+from django_filters.rest_framework import FilterSet, BooleanFilter
+
+from ..models import User
+
+
+class ProductFilter(FilterSet):
+    locked = BooleanFilter(name='veterinarian__locked')
+    verified = BooleanFilter(name='veterinarian__verified')
+
+    class Meta:
+        model = User
+        fields = [
+            'username', 'email', 'full_name', 'is_active', 'groups',
+            'veterinarian__verified', 'veterinarian__locked'
+        ]
